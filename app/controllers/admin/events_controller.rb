46456417699274203f1e156752c57737d504bff4
Ryodesse <ryo13kb-13.7@docomo.ne.jp>
2018-02-15 09:26:56 +0000

    destroy fin

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,11 @@
 class Admin::EventsController < ApplicationController
   layout 'admin'
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   def index
     @events = Event.all
   end
   
-  def show
-    @event = Event.find(params[:id])
-  end
+  
   
   def new
     @event = Event.new
@@ -19,7 +18,24 @@ class Admin::EventsController < ApplicationController
   redirect_to admin_event_path(@event), notice: 'Event was successfully created.'
   end
   
-  def edit
+  
+  
+  def update
+    
+    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event.update(event_params)
+    redirect_to admin_event_path(@event), notice: 'Event was successfully update.'
+  end
+  
+  def destroy
+    
+    @event.destroy
+    redirect_to  admin_event_path(@event), notice: 'Event was successfully destroy.'
+  end
+  
+  private
+  def set_event
+    @event = Event.find(params[:id])
   end
   
 end
